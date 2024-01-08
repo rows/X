92da1f85e30f5aebcbdaaa4fcc7328c668b1cf59
Victor Fernandes <victor.fernandes@rows.com>
2024-01-08 19:18:06 +0000

    Add feedback link

--- src/components/no-results.tsx
+++ src/components/no-results.tsx
@@ -1,12 +1,16 @@
 import './no-results.css'
 
 const NoResults = () => {
+    const redirectToFeedback = () => {
+        window.open('https://feedback.rows.com');
+    }
+
     return (
         <div className="no-results-container">
             <div className="no-results">
                 <img src="/empty.svg" />
                 <strong>No results</strong>
-                Are we missing something? <a>Request website</a> and we will make it work for you!
+                <span>Are we missing something? <a href="https://feedback.rows.com" onClick={redirectToFeedback}>Request website</a> and we will make it work for you!</span>
             </div>
         </div>
     );

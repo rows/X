79bf601fdc5f418e1c5511368fc336e13c580b4a
Victor Fernandes <victor.fernandes@rows.com>
2024-01-10 19:29:40 +0000

    chore: add HMR to the plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import preact from '@preact/preset-vite';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json'
 
-export default defineConfig({
-  plugins: [
-      preact(),
-      crx({ manifest }),
-  ],
+export default defineConfig( {
+
+        plugins: [
+            preact(),
+            crx({
+                manifest
+            }),
+        ],
+
 })

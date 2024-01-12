e98f66c7040f69f4f05cae2cc97dfa64787e1bbf
Victor Fernandes <victor.fernandes@rows.com>
2024-01-12 12:36:39 +0000

    fix: better parsing on product hunt

--- src/background.ts
+++ src/background.ts
@@ -14,7 +14,7 @@ function getScrapperOptionsByUrl(url: string, title: string) {
             ]
         };
     }
-    
+
     if (url.includes('ycombinator.com/companies')) {
         return {
             header: 'YCombinator results',
@@ -113,7 +113,7 @@ function getScrapperOptionsByUrl(url: string, title: string) {
             elementParser: [
                 { title: 'Product image', query: 'img,video', type: 'image' },
                 { title: 'Product name', query: '[data-test*="post-name"], a[href*="/products"] div:nth-child(1)', type: 'text' },
-                { title: 'Description', query: '[class*="styles_tagline"], a[href*="/products"] div:nth-child(2)', type: 'text' },
+                { title: 'Description', query: '[class*="styles_tagline"], a[href*="/products"] div:nth-child(2), [class*="_extraInfo"]', type: 'text' },
                 { title: 'Up votes', query: '[data-test="vote-button"]', type: 'text' },
                 { title: 'Product hunt link', query: 'a[data-test*="post-name"], a[href*="/products"]', type: 'link' },
             ]

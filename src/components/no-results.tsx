d3531273fa18f9668756655ac5a03faeb7a572c6
Victor Fernandes <victor.fernandes@rows.com>
2024-01-12 11:47:57 +0000

    feat: add mailto to [Talk to us] button

--- src/components/no-results.tsx
+++ src/components/no-results.tsx
@@ -1,8 +1,15 @@
 import './no-results.css'
+import Button from "./button.tsx";
+import {getCurrentTab} from "../utils/chrome.ts";
 
 const NoResults = () => {
-    const redirectToFeedback = () => {
-        window.open('https://feedback.rows.com');
+    const redirectToFeedback = async () => {
+        const tab = await getCurrentTab();
+        const url = new URL(tab.url!);
+        const message = "Hi Rows team,\n\n"+
+            `I'd like to add "${tab.url}" to be compatible with RowsX.\n\nBest,\n\n--`;
+
+        window.open(`mailto:enterprise@rows.com?subject=Request RowsX to be compatible with ${url.origin}&body=${encodeURIComponent(message)}`);
     }
 
     return (
@@ -10,7 +17,12 @@ const NoResults = () => {
             <div className="no-results">
                 <img src="/empty.svg" />
                 <strong>No results</strong>
-                <span>Are we missing something? <a href="https://feedback.rows.com" onClick={redirectToFeedback}>Request website</a> and we will make it work for you!</span>
+                <span>Would you like RowsX to support this website?</span>
+                <div className="btn-container">
+                    <Button type="primary" onClick={redirectToFeedback}>
+                        Talk to us
+                    </Button>
+                </div>
             </div>
         </div>
     );

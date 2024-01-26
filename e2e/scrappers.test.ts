36f952cb09a8e50252dd3e319bb822f7938708a7
Victor Fernandes <victor.fernandes@rows.com>
2024-01-26 18:50:22 +0000

    feat: add e2e to pipeline (#27)

--- e2e/scrappers.test.ts
+++ e2e/scrappers.test.ts
@@ -1,49 +1,70 @@
 import { readdirSync, promises as fs } from 'fs';
 import { resolve } from 'path';
-
-const extensionId = 'phpjngkjjocinjepokdimcomfmgkogbe';
-const extensionUrl = `chrome-extension://${extensionId}/index.html`;
+import * as yaml from 'js-yaml';
 
 function listDirectories(path: string) {
   const directories = readdirSync(path, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
+    .filter((dir) => dir.isDirectory() && dir.name !== '__snapshots__')
     .map((dir) => dir.name);
 
   return directories;
 }
 
+async function getExtensionId() {
+  const workerTarget = await browser.waitForTarget((target) => target.type() === 'service_worker');
+
+  const urlRegex = /chrome-extension:\/\/(?<id>[a-z]+)/;
+  const match = urlRegex.exec(workerTarget.url());
+
+  if (!match || !match.groups) {
+    throw new Error('Extension URL does not match expected format');
+  }
+
+  return match.groups.id;
+}
+
 describe('RowsX - scrappers tests', () => {
   const tests = listDirectories(__dirname);
+  let extensionId = '';
+
+  beforeAll(async () => {
+    extensionId = await getExtensionId();
+  });
 
   it.each(tests)('Scrapping - %s', async (domain) => {
-    const appPage = await browser.newPage();
+    // 1) get info about the e2e test
+    const specData = await fs.readFile(resolve(__dirname, `./${domain}/test.yml`));
+    const spec = yaml.load(specData.toString()) as { url: string; result: string };
+    const extensionUrl = `chrome-extension://${extensionId}/index.html`;
     const data = await fs.readFile(resolve(__dirname, `./${domain}/index.html`));
 
+    // 2) open website to scrap
+    const appPage = await browser.newPage();
+    const extensionPage = await browser.newPage();
+
+    // 3) mock the requests
     await appPage.setRequestInterception(true);
 
     appPage.on('request', async (request) => {
-      if (request.url().endsWith('/')) {
-        request.respond({ status: 200, contentType: 'text/html', body: data });
-      } else {
-        request.continue();
-      }
+      request.respond({ status: 200, contentType: 'text/html', body: data.toString() });
     });
 
-    const extensionPage = await browser.newPage();
     await appPage.bringToFront();
-    await appPage.goto(`https://${domain}`, { waitUntil: 'domcontentloaded' });
+    await appPage.goto(spec.url, { waitUntil: 'domcontentloaded' });
     await extensionPage.goto(extensionUrl, { waitUntil: 'domcontentloaded' });
-    await appPage.waitForTimeout(1000);
+    await appPage.waitForTimeout(250);
     await extensionPage.bringToFront();
     const button = await extensionPage.waitForSelector('.copy-btn');
     await button.click();
     await appPage.bringToFront();
 
     const context = await browser.defaultBrowserContext();
-    await context.overridePermissions(`https://${domain}`, ['clipboard-read']);
+    await context.overridePermissions(spec.url, ['clipboard-read']);
     const clipboard = await appPage.evaluate(() => navigator.clipboard.readText());
+
+    // close pages
     await appPage.close();
-    const result = await fs.readFile(resolve(__dirname, `./${domain}/result.tsv`));
-    expect(clipboard).toBe(result.toString().trimEnd());
+
+    expect(clipboard.trimEnd()).toMatchSnapshot();
   });
 });

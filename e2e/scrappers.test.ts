2e8804c07ea70d08b3486d070a9b7bf9035d1076
Victor Fernandes <victor.fernandes@rows.com>
2024-01-26 17:08:03 +0000

    Add idealista e2e tests

--- e2e/scrappers.test.ts
+++ e2e/scrappers.test.ts
@@ -1,5 +1,6 @@
 import { readdirSync, promises as fs } from 'fs';
 import { resolve } from 'path';
+import * as yaml from 'js-yaml';
 
 function listDirectories(path: string) {
   const directories = readdirSync(path, { withFileTypes: true })
@@ -31,6 +32,8 @@ describe('RowsX - scrappers tests', () => {
   });
 
   it.each(tests)('Scrapping - %s', async (domain) => {
+    const specData = await fs.readFile(resolve(__dirname, `./${domain}/test.yml`));
+    const spec = yaml.load(specData.toString()) as { url: string; result: string };
     const extensionUrl = `chrome-extension://${extensionId}/index.html`;
     const appPage = await browser.newPage();
     const data = await fs.readFile(resolve(__dirname, `./${domain}/index.html`));
@@ -41,22 +44,22 @@ describe('RowsX - scrappers tests', () => {
       if (request.url().endsWith('/')) {
         request.respond({ status: 200, contentType: 'text/html', body: data });
       } else {
-        request.continue();
+        request.abort();
       }
     });
 
     const extensionPage = await browser.newPage();
     await appPage.bringToFront();
-    await appPage.goto(`https://${domain}`, { waitUntil: 'domcontentloaded' });
+    await appPage.goto(spec.url, { waitUntil: 'domcontentloaded' });
     await extensionPage.goto(extensionUrl, { waitUntil: 'domcontentloaded' });
-    await appPage.waitForTimeout(1000);
+    await appPage.waitForTimeout(200);
     await extensionPage.bringToFront();
     const button = await extensionPage.waitForSelector('.copy-btn');
     await button.click();
     await appPage.bringToFront();
 
     const context = await browser.defaultBrowserContext();
-    await context.overridePermissions(`https://${domain}`, ['clipboard-read']);
+    await context.overridePermissions(spec.url, ['clipboard-read']);
     const clipboard = await appPage.evaluate(() => navigator.clipboard.readText());
     await appPage.close();
     const result = await fs.readFile(resolve(__dirname, `./${domain}/result.tsv`));

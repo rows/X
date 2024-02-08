4fed56a115c2a71388a25a6bd2615df72e646454
Victor Fernandes <victor.fernandes@rows.com>
2024-02-08 15:00:49 +0000

    feat: async features (#42)
    * Add skeleton
    
    * fix e2e flakiness
    
    * gfix e2e flakiness

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -1,12 +1,10 @@
 module.exports = {
   launch: {
     dumpio: false,
-    headless:  'new',
+    headless: 'new',
     args: [
       '--disable-extensions-except=./dist',
       '--load-extension=./dist',
-      '--disable-gpu',
-      '--no-sandbox',
     ],
   },
 };
---
+++ src/components/loading-skeleton.tsx
@@ -0,0 +1,62 @@
+function randomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const LoadingSkeleton = () => {
+  return (
+    <div className="skeleton-results">
+      <div className="table-preview" style={{ opacity: 0.5 }}>
+        <div className="table-header">
+          <caption
+            className="title skeleton"
+            style={{ background: 'var(--light-grey)', width: 150, height: 14 }}
+          />
+          <div className="pill skeleton" style={{ height: 20, width: 60 }}></div>
+        </div>
+        <div className="table-body">
+          <div className="table-container">
+            <table>
+              {new Array(5).fill(0).map((_, index) => (
+                <tr key={index}>
+                  <td>
+                    <div
+                      className="skeleton"
+                      style={{
+                        background: 'var(--light-grey)',
+                        width: randomNumber(30, 80),
+                        height: 14,
+                      }}
+                    />
+                  </td>
+                  <td>
+                    <div
+                      className="skeleton"
+                      style={{
+                        background: 'var(--light-grey)',
+                        width: randomNumber(30, 80),
+                        height: 14,
+                      }}
+                    />
+                  </td>
+                  <td>
+                    <div
+                      className="skeleton"
+                      style={{
+                        background: 'var(--light-grey)',
+                        width: randomNumber(30, 80),
+                        height: 14,
+                      }}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </table>
+            <div className="shade" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSkeleton;

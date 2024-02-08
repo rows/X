4faf0063a66bdda5aabce01c8dc0b393e944fed0
Victor Fernandes <victor.fernandes@rows.com>
2024-02-08 14:50:48 +0000

    gfix e2e flakiness

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   launch: {
     dumpio: false,
-    headless: false,
+    headless: 'new',
     args: [
       '--disable-extensions-except=./dist',
       '--load-extension=./dist',

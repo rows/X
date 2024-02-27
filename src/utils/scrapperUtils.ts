c95fa98fcbca5db440a03a024dc0b88494973ed8
Ricardo Felgueiras <armindofelgueiras@gmail.com>
2024-02-27 10:58:56 +0000

    fix: fix: move url pattern matching and best scrapper to different files

---
+++ src/utils/scrapperUtils.ts
@@ -0,0 +1,36 @@
+import { ScrapperOptions, getDomainName } from './chrome';
+import { urlMatchesPatternUrl } from './urlUtils';
+import scrapperOptions from './../scrappers';
+
+export function getScrapperOptionsByUrl(url: string, title: string): ScrapperOptions | null {
+  let options;
+
+  const domain = getDomainName(url);
+
+  if (domain && scrapperOptions.has(domain)) {
+    const scrappers = scrapperOptions.get(domain)!;
+
+    const scrapper = scrappers.find((scrapper) => {
+      if (Array.isArray(scrapper.url)) {
+        return scrapper.url.some((scrapperURL: string) => urlMatchesPatternUrl(url, scrapperURL));
+      } else {
+        return urlMatchesPatternUrl(url, scrapper.url);
+      }
+    });
+
+    options = scrapper;
+  }
+
+  if (options) {
+    if (!options.header) {
+      return {
+        header: title,
+        ...options,
+      };
+    }
+
+    return options;
+  }
+
+  return null;
+}

05d047b99101283bc615d1ba52f8d553d0d11f34
Crocodile Bounty Hunter <167210515+crocodile-bounty-hunter@users.noreply.github.com>
2024-04-17 18:12:52 +0100

    add report button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,13 @@ import Button from './button';
 const Header = (): JSX.Element => {
   return (
     <header className="header">
-      <img src="/logo.svg" />
-      <Button size="small" onClick={() => window.close()}>
-        <img src="/icons/close.svg" />
-      </Button>
+      <img src="/logo.svg"/>
+      <div class="options">
+        <Button type="text" size="small" onClick={() => 0}>Report</Button>
+        <Button size="small" onClick={() => window.close()}>
+          <img src="/icons/close.svg"/>
+        </Button>
+      </div>
     </header>
   );
 };

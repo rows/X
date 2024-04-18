de8692d0d5637c8b4ebd6321e51fc645eddf91d3
Crocodile Bounty Hunter <167210515+crocodile-bounty-hunter@users.noreply.github.com>
2024-04-18 18:46:55 +0100

    fix

--- src/components/no-results.tsx
+++ src/components/no-results.tsx
@@ -1,85 +1,32 @@
+import { JSX } from 'preact';
+import './no-results.css';
+import { createNewReportEntryRow} from '../utils/rows-api/report';
 import Button from './button';
-import './preview.css';
-import { array2tsv, hasImage } from '../utils/copy';
-import { FunctionComponent } from 'preact';
-import { ScrapperResults } from '../utils/chrome';
-
-function renderCell(cell: string) {
-  if (hasImage(cell)) {
-    return <img alt="rows_x_image" src={cell} />;
-  }
-
-  return cell;
-}
-
-async function copyToClipboard(result: {
-  title?: string;
-  table: string[][];
-  includeHeader?: boolean;
-}) {
-  let tableToCopy = result.table;
-  if (!result.includeHeader) {
-    tableToCopy = result.table.slice(1); // Remove the first row (header)
-  }
-
-  await navigator.clipboard.writeText(array2tsv(tableToCopy));
-  setTimeout(() => window.close(), 200);
-}
 
 interface Props {
-  results: ScrapperResults;
+  message?: string;
 }
 
-const Preview: FunctionComponent<Props> = ({ results = [] }) => {
+const NoResults = ({ message }: Props): JSX.Element => {
+  const redirectToFeedback = () => {
+    setTimeout(() => window.close(), 150);
+    createNewReportEntryRow();
+  };
+
   return (
-    <div className="results">
-      {results.map((result) => {
-        return (
-          <div className="table-preview">
-            <div className="table-header">
-              <caption className="title">{result.title}</caption>
-              <div className="pill">{`${result.table.length - 1} records`}</div>
-            </div>
-            <div className="table-body">
-              <div className="table-container">
-                <table>
-                  {result.table.slice(0, 6).map((row) => (
-                    <tr>
-                      {row.map((col) => (
-                        <td>{renderCell(col)}</td>
-                      ))}
-                    </tr>
-                  ))}
-                </table>
-                {result.table.length > 5 && <div className="shade" />}
-              </div>
-              <div className="table-actions">
-                <Button
-                  className="copy-btn"
-                  variant="secondary"
-                  onClick={() => copyToClipboard(result)}
-                >
-                  <img alt="copy" src="/icons/copy.svg" />
-                </Button>
-                <Button
-                  className="open-rows-btn"
-                  variant="primary"
-                  onClick={() => {
-                    chrome.runtime.sendMessage({
-                      action: 'rows-x:store',
-                      data: array2tsv(result.table),
-                    });
-                  }}
-                >
-                  Open in Rows
-                </Button>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+    <div className="no-results-container">
+      <div className="no-results">
+        <img src="/empty.svg" />
+        <strong>No results</strong>
+        <span>{message ?? 'Would you like RowsX to support this website?'}</span>
+        <div className="btn-container">
+          <Button variant="primary" onClick={redirectToFeedback}>
+            Report
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Preview;
+export default NoResults;
